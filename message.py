f1e9c3fb57cc8c785efda50ed2b1121f20d7c111
krishnh <60828323+krishnh@users.noreply.github.com>
2020-06-14 00:59:21 -0400

    Update message.py

--- message.py
+++ message.py
@@ -4,5 +4,5 @@ s.starttls()
 s.login("jyotididwania636@gmail.com","Milindverma@123")
 message=("MODEL BUILT CORRECTLY !!!")
 s.sendmail("jyotididwania636@gmail.com","hemant.krishnatrey@gmail.com",message)
-
+//a
 
