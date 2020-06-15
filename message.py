33fe1308feb0f79428d4e59fe2b2cf77ae014cb5
krishnh <60828323+krishnh@users.noreply.github.com>
2020-06-15 16:04:54 -0400

    Update message.py

--- message.py
+++ message.py
@@ -1,8 +1,8 @@
 import smtplib
 s=smtplib.SMTP("smtp.gmail.com",587)
 s.starttls()
-s.login("jyotididwania636@gmail.com","Milindverma@123")
+s.login("michaelverma0141@gmail.com","michaelverma16034619")
 message=("MODEL BUILT CORRECTLY !!!")
-s.sendmail("jyotididwania636@gmail.com","hemant.krishnatrey@gmail.com",message)
+s.sendmail("michaelverma01416@gmail.com","hemant.krishnatrey@gmail.com",message)
 
 
